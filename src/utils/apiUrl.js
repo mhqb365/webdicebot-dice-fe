1dd094cbc9c27f8ca6bf044861f22a02c47d47e4
mhqb365 <mhqb365@gmail.com>
2020-11-30 14:58:16 +0700

    new-deploy

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,6 +1,6 @@
 const URL = {
     DEV: 'http://localhost:8003',
-    PROD: 'https://dice-be.webdicebot.xyz'
+    PROD: 'https://tronfree-be.webdicebot.xyz'
 }
 
 const API_URL = window.location.hostname == 'localhost' ? URL.DEV : URL.PROD
